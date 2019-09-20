16fd5d45c202b5292f77fc8f8886f77ef99df1e6
lagrassanicolas <la.grassa.nicolas@iisgalvanimi.edu.it>
2019-09-20 08:54:29 +0000

    punto1

---
+++ request-http/src/app/foo/foo.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+ selector: 'app-foo',
+ templateUrl: './foo.component.html',
+ styleUrls: ['./foo.component.css']
+})
+export class FooComponent {
+   data: Object;
+   loading: boolean;
+   o :Observable<Object>;
+   constructor(public http: HttpClient) {}
+   makeRequest(): void {
+     console.log("here");
+     this.loading = true;
+     this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/1');
+     this.o.subscribe(this.getData);
+   }
+   getData = (d : Object) =>
+   {
+     this.data = new Object(d);
+     this.loading = false;
+   }
+}
